9e31e48b520b50f0fc007f791fe6b3815b2987c7
rorycl <rorycl@campbell-lange.net>
2026-01-27 22:03:25 +0000

    internal: add embed and directory fs.FS mounting module

---
+++ internal/mounts.go
@@ -0,0 +1,169 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// FileMount is a mount that may be mounted by either an embedded fs.FS or a filePath.
+type FileMount struct {
+	MountName string
+	fs.FS
+}
+
+// String describes a fileMount as a list of files and directories indented by the file
+// or directory level.
+func (fm FileMount) String() string {
+	o := fmt.Sprintf("fileMount %q:\n", fm.MountName)
+	s, _ := PrintFS(fm.FS)
+	return o + s
+}
+
+// NewFileMount takes an embedded fs.FS or a path to a directory. If the path to the
+// directory is "", the embedded fs is used, otherwise the directory is used. Note that
+// the MountName parameter is used to name the mount of an fs.FS to make it work like an
+// os.DirFS.
+//
+// In other words, given a directory "templates" and a go embed fs.FS called templateFS
+// such as:
+//
+//	//go:embed "path/templates"
+//	var templatesFS fs.FS
+//
+// and a `NewFileMount` invocation such as
+//
+//	NewFileMount("path/templates", templatesFS, "")
+//
+// will mount the embedded fs.FS at the equivalent of "path/templates/" rather than ".",
+// giving the impression of moving the fs level up one.
+//
+// On the other hand an invocation as follows:
+//
+//	NewFileMount("path/templates", templatesFS, "path/templates")
+//
+// will mount the filesystem location "templates" to "templates/".
+func NewFileMount(MountName string, embeddedFS fs.FS, dirPath string) (*FileMount, error) {
+
+	if MountName == "" {
+		return nil, errors.New("no MountName provided for new file mount")
+	}
+
+	// If a directory is not provided, use the embedded fs, but mounted at the
+	// subdirectory provided at MountName.
+	if dirPath == "" {
+		subFS, err := fs.Sub(embeddedFS, MountName)
+		if err != nil {
+			return nil, fmt.Errorf("could not sub-mount embedded fs at %q: %v", MountName, err)
+		}
+		return &FileMount{
+			MountName,
+			subFS,
+		}, nil
+	}
+
+	s, err := os.Stat(dirPath)
+	if err != nil {
+		return nil, fmt.Errorf("new mount at %q error: %s", dirPath, err)
+	}
+	if !s.IsDir() {
+		return nil, fmt.Errorf("new mount at %q is not a directory", dirPath)
+	}
+
+	dirFS := os.DirFS(dirPath)
+	return &FileMount{
+		MountName,
+		dirFS,
+	}, nil
+}
+
+// Materialize outputs the data in fm.FS recursively to the filesystem starting at
+// root. For example running:
+//
+//	nf, _ := NewFileMount("templates", templatesFS, "")
+//	_ = nf.Materialize("/tmp/")
+//
+// will create the contents of templatesFS in "/tmp/templates". Root must be a
+// directory.
+func (fm *FileMount) Materialize(root string) error {
+
+	checkIsDir := func(path string) error {
+		s, err := os.Stat(path)
+		if err != nil {
+			var fspe *fs.PathError // https://go.dev/wiki/ErrorValueFAQ
+			if errors.As(err, &fspe) {
+				return errors.New("was not found")
+			}
+			return err
+		}
+		if !s.IsDir() {
+			return errors.New("is not a directory")
+		}
+		return nil
+	}
+
+	// Check the root exists and intended file path does not already exist.
+	if err := checkIsDir(root); err != nil {
+		return fmt.Errorf("materialize root %q invalid: %v", root, err)
+	}
+
+	// Recurse, writing files and making directories from the fs.FS.
+	err := fs.WalkDir(fm.FS, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err // error propogation
+		}
+		fullPath := filepath.Join(root, path)
+
+		if d.IsDir() {
+			err := os.MkdirAll(fullPath, 0755)
+			if err != nil {
+				return fmt.Errorf("could not make dir %q, %v", fullPath, err)
+			}
+			return nil
+		}
+		if !d.Type().IsRegular() {
+			// not a regular file
+			return nil
+		}
+
+		data, err := fs.ReadFile(fm.FS, path)
+		if err != nil {
+			return fmt.Errorf("could not read %q from mount %s: %v", path, fm.MountName, err)
+		}
+		err = os.WriteFile(fullPath, data, 0644)
+		if err != nil {
+			return fmt.Errorf("could not write %q at %q from mount %s: %v", path, fullPath, fm.MountName, err)
+		}
+		return nil
+	})
+	return err
+}
+
+// PrintFS makes structured print output from an fs.FS.
+func PrintFS(thisFS fs.FS) (string, error) {
+	var printOutput strings.Builder
+	err := fs.WalkDir(thisFS, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err // propogate
+		}
+		tpl := "%s[%s] %s%s (%s)\n"
+		depth := strings.Count(path, string(os.PathSeparator))
+		indent := strings.Repeat("  ", depth)
+		typer := "f"
+		name := d.Name()
+		if path == "." {
+			name = "." // special case for the root.
+		}
+		slash := " "
+		if d.IsDir() {
+			slash = string(os.PathSeparator)
+			typer = "d"
+		}
+		_, err = printOutput.WriteString(fmt.Sprintf(tpl, indent, typer, name, slash, path))
+		return err
+	})
+	return printOutput.String(), err
+}
---
+++ internal/mounts_test.go
@@ -0,0 +1,118 @@
+package internal
+
+import (
+	"embed"
+	_ "embed"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+//go:embed testdata
+var testdata embed.FS
+
+//go:embed testdata/dirA
+var testdataDirA embed.FS
+
+func TestMounts(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		mountName  string
+		embeddedFS fs.FS
+		dirPath    string
+		dirToStat  string
+		err        error
+	}{
+		{
+			name:       "embedded fs mount",
+			mountName:  "testdata",
+			embeddedFS: testdata,
+			dirPath:    "",
+			dirToStat:  "dirA/dirB",
+		},
+		{
+			name:       "directory fs mount",
+			mountName:  "testdata",
+			embeddedFS: testdata,
+			dirPath:    "./testdata",
+			dirToStat:  "dirA/dirB",
+		},
+		{
+			name:       "directory fs mount fail",
+			mountName:  "testdata",
+			embeddedFS: testdata,
+			dirPath:    "./doesNotExist",
+			err:        errors.New(`new mount at "./doesNotExist"`),
+		},
+		{
+			name:       "embedded fs mount for dirA",
+			mountName:  "testdata/dirA",
+			embeddedFS: testdataDirA,
+			dirPath:    "",
+			dirToStat:  "dirB",
+		},
+		{
+			name:       "directory fs mount for dirA",
+			mountName:  "testdata/dirA",
+			embeddedFS: testdataDirA,
+			dirPath:    "testdata/dirA",
+			dirToStat:  "dirB",
+		},
+	}
+
+	for ii, tt := range tests {
+		t.Run(fmt.Sprintf("%d_%s", ii, tt.name), func(t *testing.T) {
+
+			testDir := t.TempDir()
+			// uncomment to inspect directory
+			// testDir, err := os.MkdirTemp("", "mount_*")
+			// if err != nil {
+			// 	t.Fatal(err)
+			// }
+
+			fm, err := NewFileMount(tt.mountName, tt.embeddedFS, tt.dirPath)
+			if err != nil {
+				if tt.err != nil {
+					if got, want := err.Error(), tt.err.Error(); !strings.Contains(got, want) {
+						fmt.Errorf("error got %q want substring %q", got, want)
+					}
+					return
+				}
+				t.Fatalf("unexpected New error %v", err)
+			}
+
+			stat, err := fs.Stat(fm.FS, tt.dirToStat)
+			if err != nil {
+				t.Fatalf("could not find dir 'dirB' in 'dirA' at top level of fs: %v", err)
+			}
+			if !stat.IsDir() {
+				t.Errorf("dir 'dirB' in 'dirA' of fs is not a dir: %v", stat.Name())
+			}
+			err = fm.Materialize(testDir)
+			if err != nil {
+				t.Errorf("unexpected error %v", err)
+			}
+
+			materializedFSAsString, err := PrintFS(os.DirFS(testDir))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			mountFSAsString, err := PrintFS(fm.FS)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(materializedFSAsString, mountFSAsString); diff != "" {
+				t.Errorf("unexpected difference between materialization and mount:\n%s", diff)
+			}
+
+		})
+	}
+}
